f82d3e691a77c81391363bd98dc5f44aaa6212bb
Tushar Bhatt <tusharbhatt0135@gmail.com>
2024-01-24 20:23:48 +0530

    navbar - border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className='flex items-center font-extrabold justify-between shadow-md border-b border-slate-50 p-4 px-7 text-blue-700 '>
+    <div className='flex items-center font-extrabold justify-between shadow-md border-b border-slate-700 p-4 px-7 text-blue-700 '>
       <h1 className='text-4xl '>Sketch.io</h1>
       <p className="text-white">Logic &gt;&gt;&gt; Design</p>
     </div>
